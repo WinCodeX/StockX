c8d0136dc05bedaf34b39ad2a35d4e7f07cc8409
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-20 12:58:31 +0300

    Update sales_controller.rb

--- app/controllers/api/v1/sales_controller.rb
+++ app/controllers/api/v1/sales_controller.rb
@@ -32,13 +32,7 @@ module Api
 
       private
 
-      def authenticate_request
-        header = request.headers['Authorization']
-        token = header.split.last if header
-        decoded = JsonWebToken.decode(token)
-        @current_user = User.find_by(id: decoded[:user_id]) if decoded
-        render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
-      end
+     
 
       def set_product
         @product = Product.find(params[:product_id])
