124cfaa0295688c859deb2044a72b383359cd7c7
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-21 09:40:05 +0300

    Update me_controller.rb

--- app/controllers/api/v1/me_controller.rb
+++ app/controllers/api/v1/me_controller.rb
@@ -4,14 +4,22 @@ module Api
       before_action :authenticate_user!
 
       def show
-        render json: { id: @current_user.id, name: @current_user.name, email: @current_user.email, avatar: @current_user.avatar }
+        render json: {
+          id: current_user.id,
+          name: current_user.name,
+          email: current_user.email,
+          avatar: current_user.avatar
+        }
       end
 
       def update_avatar
-        if @current_user.update(avatar: params[:avatar])
-          render json: { message: 'Avatar updated successfully', avatar: @current_user.avatar }
+        if current_user.update(avatar: params[:avatar])
+          render json: {
+            message: 'Avatar updated successfully',
+            avatar: current_user.avatar
+          }
         else
-          render json: { errors: @current_user.errors.full_messages }, status: :unprocessable_entity
+          render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
         end
       end
     end
