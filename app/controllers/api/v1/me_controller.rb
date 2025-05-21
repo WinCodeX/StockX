67e227b6b5d6e861e22fee64b54cd714b70a1d7c
WinCodeX <glenwinterg970@gmail.com>
2025-05-21 10:13:07 +0300

    New fix

--- app/controllers/api/v1/me_controller.rb
+++ app/controllers/api/v1/me_controller.rb
@@ -8,6 +8,7 @@ module Api
 def show
   render json: {
     id: current_user.id,
+    email: current_user.username,
     email: current_user.email,
     avatar: current_user.avatar.attached? ? url_for(current_user.avatar) : nil
   }
