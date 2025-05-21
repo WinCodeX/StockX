feaddacbd1b142ec3e2975d4117f4d4d22aa2c31
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-21 09:56:02 +0300

    Update me_controller.rb

--- app/controllers/api/v1/me_controller.rb
+++ app/controllers/api/v1/me_controller.rb
@@ -3,13 +3,15 @@ module Api
     class MeController < ApplicationController
       before_action :authenticate_user!
 
-      def show
-        render json: {
-          id: current_user.id,
-          email: current_user.email,
-          avatar: current_user.avatar
-        }
-      end
+      include Rails.application.routes.url_helpers
+
+def show
+  render json: {
+    id: current_user.id,
+    email: current_user.email,
+    avatar: current_user.avatar.attached? ? url_for(current_user.avatar) : nil
+  }
+end
 
       def update_avatar
         if current_user.update(avatar: params[:avatar])
