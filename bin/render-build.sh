5ee9729fd4d78c5f3e068a576f13cabc78a80a44
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-25 09:19:34 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,21 @@
 set -o errexit
 
 bundle install
-./bin/rails assets:precompile
-./bin/rails assets:clean
+
+# ./bin/rails assets:precompile
+# ./bin/rails assets:clean
+
+# echo "=== Dropping and Creating Database ==="
+# bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1 || true
+# bundle exec rails db:create
+      
+
+echo "=== Seeding Test Data ==="
+bundle exec rails db:seed
+
+bin/rails db:migrate
+
+
+echo "=== Build Complete Successfully ==="
+
+#  bundle install; bundle exec rails assets:precompile; bundle exec rails assets:clean;
