c4228e2a170f5fbb83cdadc86e8758dec2633fa2
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-25 16:49:05 +0300

    Update active_storage_disable_checksum.rb

--- config/initializers/active_storage_disable_checksum.rb
+++ config/initializers/active_storage_disable_checksum.rb
@@ -1,4 +1,4 @@
-# config/initializers/active_storage_disable_checksum.rb
+# config/initializers/active_storage_patch.rb
 
 Rails.application.config.after_initialize do
   ActiveSupport.on_load(:active_storage_service) do
@@ -8,7 +8,12 @@ Rails.application.config.after_initialize do
       def upload(key, io, checksum: nil, **options)
         instrument :upload, key: key, checksum: checksum do
           object = object_for(key)
+
+          # Disable all checksum handling for compatibility with Cloudflare R2
           options = options.merge(body: io)
+          options.delete(:content_md5)
+          options.delete(:checksum_algorithm)
+
           object.put(options)
         end
       end
