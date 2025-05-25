de0f0270f474261588adeec434910bc42c143150
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-25 16:42:09 +0300

    Update active_storage_disable_checksum.rb

--- config/initializers/active_storage_disable_checksum.rb
+++ config/initializers/active_storage_disable_checksum.rb
@@ -1,12 +1,16 @@
 # config/initializers/active_storage_disable_checksum.rb
 
 Rails.application.config.after_initialize do
-  ActiveStorage::Service::S3Service.class_eval do
-    def upload(key, io, checksum: nil, **options)
-      instrument :upload, key: key, checksum: checksum do
-        object = object_for(key)
-        options = options.merge(body: io)
-        object.put(options)
+  ActiveSupport.on_load(:active_storage_service) do
+    require "active_storage/service/s3_service"
+
+    ActiveStorage::Service::S3Service.class_eval do
+      def upload(key, io, checksum: nil, **options)
+        instrument :upload, key: key, checksum: checksum do
+          object = object_for(key)
+          options = options.merge(body: io)
+          object.put(options)
+        end
       end
     end
   end
