f219e1439fb895f18674dac74f72e6de7660a611
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-28 22:38:02 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,10 +33,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-
-# config/environments/development.rb
-
-Rails.application.routes.default_url_options[:host] = ENV.fetch('RAILS_HOST', 'http://192.168.149.106:3000')
+Rails.application.routes.default_url_options[:host] = 'http://192.168.100.73:3000'
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
