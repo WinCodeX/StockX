9c9cf2f232a3027ed9e1ac4ebc55bc0211d7442c
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-30 15:58:52 +0300

    Update invite.rb
    qr code generator

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,4 +1,15 @@
 class Invite < ApplicationRecord
-  belongs_to :inviter
+  belongs_to :inviter, class_name: 'User'
   belongs_to :business
+
+  before_create :generate_unique_code
+
+  private
+
+  def generate_unique_code
+    loop do
+      self.code = SecureRandom.hex(4)
+      break unless Invite.exists?(code: code)
+    end
+  end
 end
