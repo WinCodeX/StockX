9887897ae6193579ec2af7dc355e623742b147bd
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-30 23:11:03 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,16 +10,16 @@ bundle install
 # echo "=== Dropping and Creating Database ==="
 # bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1 || true
 # bundle exec rails db:create
-      
+bundle exec rails db:migrate:up VERSION=20250530070004
+
+bundle exec rails db:migrate:up VERSION=20250530070651
+
+bundle exec rails db:migrate:up VERSION=20250530081845
 
 echo "=== Seeding Test Data ==="
 bundle exec rails db:seed
 
-bin/rails db:migrate:up VERSION=20250530070004
 
-bin/rails db:migrate:up VERSION=20250530070651
-
-bin/rails db:migrate:up VERSION=20250530081845
 #bin/rails db:migrate
 
 bundle exec rails db:migrate 
