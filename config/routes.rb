d5a7ea7ceb38b020ac0737b2eb3692be68731269
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-30 18:36:25 +0300

    Update routes.rb
    add business creation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,51 @@
 Rails.application.routes.draw do
   devise_for :users,
-  path: '',
-  path_names: {
-    sign_in: 'login',
-    sign_out: 'logout'
-  },
-  controllers: {
-    sessions: 'users/sessions'
-  }
-
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout'
+    },
+    controllers: {
+      sessions: 'users/sessions'
+    }
 
   namespace :api do
-  namespace :v1 do
-    get 'me', to: 'me#show', defaults: { format: :json }
-    put 'me/avatar', to: 'me#update_avatar'
-    get 'ping', to: 'status#ping', defaults: { format: :json }
-    resources :products, defaults: { format: :json } do
-      collection do
-        get :stats
+    namespace :v1 do
+      # User routes
+      get 'me', to: 'me#show', defaults: { format: :json }
+      put 'me/avatar', to: 'me#update_avatar'
+      get 'ping', to: 'status#ping', defaults: { format: :json }
+
+      # Product + related data
+      resources :products, defaults: { format: :json } do
+        collection do
+          get :stats
+        end
+        resources :stocks, only: [:index, :create], defaults: { format: :json }
+        resources :sales, only: [:index, :create], defaults: { format: :json }
       end
 
-      resources :stocks, only: [:index, :create], defaults: { format: :json }
-      resources :sales, only: [:index, :create], defaults: { format: :json }
-    end
-
-    resources :sales, only: [], defaults: { format: :json } do
-      collection do
-        get :recent
+      # Sales
+      resources :sales, only: [], defaults: { format: :json } do
+        collection do
+          get :recent
+        end
       end
-    end
 
-
-resources :invites, only: [:create] do
-  collection do
-    post :accept
+      # Business invites
+      resources :invites, only: [:create], defaults: { format: :json } do
+        collection do
+          post :accept
+        end
       end
+
+      # Businesses
+      resources :businesses, only: [:create, :index, :show], defaults: { format: :json }
     end
   end
-end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # root "posts#index" # Uncomment and customize if you want a root path
 end
