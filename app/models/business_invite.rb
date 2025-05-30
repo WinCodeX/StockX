2f488ac352d58e9bb4918aefdd25e85afc1af2e5
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-30 22:29:43 +0300

    Update business_invite.rb
    added random 6 digit code to invite

--- app/models/business_invite.rb
+++ app/models/business_invite.rb
@@ -1,4 +1,14 @@
 class BusinessInvite < ApplicationRecord
   belongs_to :inviter, class_name: 'User'
   belongs_to :business
+
+  before_create :generate_unique_code
+
+  private
+
+  def generate_unique_code
+    begin
+      self.code = 6.times.map { rand(0..9) }.join
+    end while self.class.exists?(code: code)
+  end
 end
