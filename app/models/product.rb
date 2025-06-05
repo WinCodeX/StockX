90c07c5c3c31683cd731f88c287d5d64a8a84a28
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-06-05 15:35:52 +0300

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
 has_many :stocks, dependent: :destroy
 has_many :sales, dependent: :destroy
 has_one_attached :image
+has_one_attached :qr_code
 
 def total_stock
   stocks.sum(:quantity)
