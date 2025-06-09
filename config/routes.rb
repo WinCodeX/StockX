10a7721f30be5afef8673f1708ff9e9528988e38
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-06-09 18:06:39 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
 devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }, controllers: { sessions: 'users/sessions' }
 
-namespace :api do namespace :v1 do
+namespace :api do 
+namespace :v1 do
 
 # User routes
   get 'me', to: 'me#show', defaults: { format: :json }
