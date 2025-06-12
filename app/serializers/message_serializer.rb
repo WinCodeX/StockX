54b6f42bfc2886a9e76aa2e4e9132720208db55f
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-06-12 09:33:46 +0300

    Update message_serializer.rb

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,8 @@
-class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :body, :read, :user_id, :conversation_id, :created_at
+# app/serializers/message_serializer.rb
+class MessageSerializer
+  include FastJsonapi::ObjectSerializer
+
+  attributes :id, :body, :read, :created_at, :user_id
 
   belongs_to :user, serializer: UserSerializer
 end
