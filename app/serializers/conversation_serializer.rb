0ef922a062aef89ec0c238b31f7ffe2d2b8681ec
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-06-12 10:58:24 +0300

    Update conversation_serializer.rb

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -1,36 +1,34 @@
-# app/serializers/conversation_serializer.rb
 class ConversationSerializer
   include FastJsonapi::ObjectSerializer
-
-  attributes :id, :updated_at
+  attributes :updated_at
 
   attribute :sender do |obj|
     {
       id: obj.sender.id,
-      username: obj.sender.username
+      username: obj.sender.username,
+      avatar_url: obj.sender.avatar.attached? ?
+        Rails.application.routes.url_helpers.rails_blob_url(obj.sender.avatar, only_path: false) : nil
     }
   end
 
   attribute :receiver do |obj|
     {
       id: obj.receiver.id,
-      username: obj.receiver.username
+      username: obj.receiver.username,
+      avatar_url: obj.receiver.avatar.attached? ?
+        Rails.application.routes.url_helpers.rails_blob_url(obj.receiver.avatar, only_path: false) : nil
     }
   end
 
-  attribute :messages do |obj, params|
-    if params[:full]
-      obj.messages.order(:created_at).map do |m|
-        {
-          id: m.id,
-          body: m.body,
-          read: m.read,
-          user_id: m.user_id,
-          created_at: m.created_at
-        }
-      end
-    else
-      []
+  attribute :messages do |obj|
+    obj.messages.order(created_at: :asc).map do |m|
+      {
+        id: m.id,
+        body: m.body,
+        read: m.read,
+        user_id: m.user_id,
+        created_at: m.created_at
+      }
     end
   end
 end
